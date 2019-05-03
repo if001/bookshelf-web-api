772663c1774d2541d5e3403a093c076d3e0790a8
issei <fujimoto>
2019-05-04 04:46:32 +0900

    errorに型をつける

--- application/usecase/book.go
+++ application/usecase/book.go
@@ -7,6 +7,7 @@ import (
 
 type BookUseCase interface {
 	BookListUseCase() (*[]model.Book, error)
+	BookFindUseCase(id int64) (*[]model.Book, service.RecodeNotFoundError)
 }
 
 type bookUseCase struct {
@@ -22,6 +23,9 @@ func NewBookUseCase(cr repository.BookRepository) BookUseCase {
 func (u *bookUseCase) BookListUseCase() (*[]model.Book, error) {
 	books, err := u.BookRepo.List()
 	return books, err
+func (u *bookUseCase) BookFindUseCase(id int64) (*[]model.Book, service.RecodeNotFoundError) {
+	book, err := u.BookRepo.Find(id)
+	return book, service.RecodeNotFoundError(err)
 }
 	return cate, err
 }
