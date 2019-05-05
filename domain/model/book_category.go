3eabf1e1e1c5f214708b2c0df20b27c508af9bae
issei <fujimoto>
2019-05-05 20:37:21 +0900

    本とディスクリプションのリレイションテーブル定義

---
+++ domain/model/book_category.go
@@ -0,0 +1,14 @@
+package model
+
+type BookCategory struct {
+	BaseModel
+	BookID      int64
+	CategoryID  int64
+	Categories  []Category
+}
+
+func (BookCategory) TableName() string {
+	return "books_categories"
+}
+
+
