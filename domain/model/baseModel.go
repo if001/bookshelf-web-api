9ebe802f51d31f5d56e399ee88e2a5d669d9e7cb
issei <fujimoto>
2019-05-06 21:11:25 +0900

    時間のデフォルト値を設定

--- domain/model/baseModel.go
+++ domain/model/baseModel.go
@@ -4,6 +4,6 @@ import "time"
 
 type BaseModel struct {
 	ID        int64 `gorm:"primary_key" sql:"AUTO_INCREMENT"`
-	CreatedAt time.Time `sql:"not null;type:date"`
-	UpdatedAt time.Time `sql:"not null;type:date"`
+	CreatedAt time.Time `gorm;"default:CURRENT_TIMESTAMP" sql:"not null;type:date"`
+	UpdatedAt time.Time `gorm;"default:CURRENT_TIMESTAMP" sql:"not null;type:date"`
 }
