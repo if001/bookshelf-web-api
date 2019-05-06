54fd43cd5155235777f7fd978265ef268e1bea82
issei <fujimoto>
2019-05-06 21:12:05 +0900

    時間のnull 許可

--- domain/model/book.go
+++ domain/model/book.go
@@ -1,17 +1,20 @@
 package model
 
-import "time"
+import (
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
+)
 
 type Book struct {
 	BaseModel
 	AccountID   int64
 	Title       string `gorm:"type:varchar(40);"`
-	AuthorID    int64
-	StartAt     time.Time
-	EndAt       time.Time
-	PublishedAt time.Time
-	NextBookID  int64
-	PrevBookID  int64
+	AuthorID    sql.NullInt64
+	StartAt     mysql.NullTime
+	EndAt       mysql.NullTime
+	PublishedAt mysql.NullTime
+	NextBookID  sql.NullInt64
+	PrevBookID  sql.NullInt64
 	Author      Author `gorm:"foreignkey:AuthorID"`
 	Categories  []Category
 	Description []Description
